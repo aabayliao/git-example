7b50d2fae3709f34fe05b46bebb9d302166f688e
aabayliao <aabayliao@gmail.com>
2023-03-03 13:53:37 +0800

    Add new file main.c

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
-print('I am Frank West')
-print('hello python & git ')
-print('Everything is good')
+print('I am Frank West')      
+print('hello python & git ')         
+print('Everything is good')   
+print('Update on 2023/3/1')  
+
+
+
+
+
+
+
+
+
